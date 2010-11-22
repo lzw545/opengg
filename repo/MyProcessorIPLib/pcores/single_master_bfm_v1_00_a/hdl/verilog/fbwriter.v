8169c9e8addd2094628c427df24f4f5a8e7be00c
cluster <lzw545@gmail.com>
2010-11-22 17:27:30 -0500

    i think fbwriter is correct.  Going home to test it

--- repo/MyProcessorIPLib/pcores/single_master_bfm_v1_00_a/hdl/verilog/fbwriter.v
+++ repo/MyProcessorIPLib/pcores/single_master_bfm_v1_00_a/hdl/verilog/fbwriter.v
@@ -19,19 +19,41 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module fbwriter(
+    fifo_data,
+    fifo_empty,
+    fifo_rd_en,
+    
+    PLB_clk,
+    IP2Bus_MstRd_Req,
+    IP2Bus_MstWr_Req,
+    IP2Bus_Mst_Addr,
+    IP2Bus_Mst_BE,
+    IP2Bus_Mst_Lock,
+    IP2Bus_Mst_Reset,
+    Bus2IP_Mst_CmdAck,
+    Bus2IP_Mst_Cmplt,
+    Bus2IP_Mst_Error,
+    Bus2IP_Mst_Rearbitrate,
+    Bus2IP_Mst_Cmd_Timeout,
+    Bus2IP_MstRd_d,
+    Bus2IP_MstRd_src_rdy_n,
+    IP2Bus_MstWr_d,
+    Bus2IP_MstWr_dst_rdy_n
     );
 
 
 parameter TMP_LEN                        = 7;
-
 parameter RAST_FBW_FIFO_LEN              = 64;
-
 parameter LINE_LEN                       = 9;
 parameter COL_LEN                        = 10;
 
+// PLB Parameters
+parameter C_MST_AWIDTH                   = 32;
+parameter C_MST_DWIDTH                   = 32;
+
 
 // FIFO interface
-input      [0 : RAST_FBW_FIFO_LEN-1]      fifo_Din;
+input      [0 : RAST_FBW_FIFO_LEN-1]      fifo_data;
 input                                     fifo_empty;
 output                                    fifo_rd_en;
 
@@ -56,7 +78,7 @@ input                                     Bus2IP_MstWr_dst_rdy_n;
 
 
 
-
+  reg      [0 : 3]                        state;
 
   // reader registers
   reg                                     go_write;
@@ -79,20 +101,20 @@ input                                     Bus2IP_MstWr_dst_rdy_n;
   
   assign IP2Bus_Mst_BE[0 : C_MST_DWIDTH/8-1] = ~('b0);
   assign IP2Bus_Mst_Lock                     = 0;
-  assign IP2Bus_Mst_Reset                    = mst_reset;
+//  assign IP2Bus_Mst_Reset                    = mst_reset;
   assign IP2Bus_MstWr_d[0 : C_MST_DWIDTH-1]  = color;
   
   
   // writer state machine
 parameter OFF_STATE=0, PRESENT_STATE=1, WAIT_FOR_ACK=2, WAIT_FOR_CMPLT=3, ERROR_RECVD=4;
 
-  always @ (posedge Bus2IP_Clk)
+  always @ (posedge PLB_clk)
     begin
       case (state)
         OFF_STATE:
           if ( Bus2IP_Mst_Error )
             state     <= ERROR_RECVD;
-          else if ( fifo_empty )
+          else if ( !fifo_empty )
             state     <= PRESENT_STATE;
           else
             state     <= OFF_STATE;
@@ -136,9 +158,20 @@ parameter OFF_STATE=0, PRESENT_STATE=1, WAIT_FOR_ACK=2, WAIT_FOR_CMPLT=3, ERROR_
       mst_wr_req = 1;
     else
       mst_wr_req = 0;  
+  
+  // dequeue from fifo on trasition from IDLE to WRITE
+  assign fifo_rd_en = ( state == OFF_STATE && !fifo_empty );
 
-  always @ *
-    if ( state ==  )
-      // fill in regs
-      
+  // reset on error
+  assign IP2Bus_Mst_Reset = ( state == ERROR_RECVD );
+  
+  // assign line and col and color regs
+  always @ (posedge PLB_clk)
+    begin
+      if ( fifo_rd_en && !fifo_empty )
+        // fifo_data is valid
+        line  <= fifo_data;
+        col   <= fifo_data;
+        color <= fifo_data;
+    end
 endmodule
